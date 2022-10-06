69f216a84d2ab54caed835ab54f56849c793e66a
weiyannan <281923452@qq.com>
2022-10-06 22:42:57 +0800

    Initial commit

---
+++ src/main/java/com/weiyannan/jenkins/JenkinsSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.weiyannan.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSpringbootDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/weiyannan/jenkins/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.weiyannan.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @Title: HelloController
+ * @Author weiyannan
+ * @Date: 2022/10/6
+ * @Version 1.0
+ **/
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/test")
+    public String sayHello(){
+        return "This is jenkens demo test";
+    }
+}
